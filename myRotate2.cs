aa9e32879f9393e400b03eaf3f674557db25e098
Dhrumil Joshi <dhrumiljoshi94@gmail.com>
2021-11-28 20:22:45 +0530

    Add files via upload

---
+++ myRotate2.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class myRotate2 : MonoBehaviour
+{
+    float rotSpeed = 8;
+
+    private void Update()
+    {
+        if(Input.touchCount == 3){
+            OnMouseDragX();
+            OnMouseDragY();
+        }
+
+    }
+
+    void OnMouseDrag()
+    {
+        if (Input.touchCount == 2)
+        {
+            float rotX = Input.GetAxis("Mouse X") * rotSpeed /2* Mathf.Deg2Rad;
+            transform.RotateAround(Vector3.up, -rotX);
+        }
+
+    }
+    void OnMouseDragX()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed / 2 * Mathf.Deg2Rad;
+        transform.RotateAround(Vector3.up, -rotX);
+
+    }
+
+    void OnMouseDragY()
+    {
+
+        float rotY = Input.GetAxis("Mouse Y") * rotSpeed / 2 * Mathf.Deg2Rad;
+        transform.RotateAround(Vector3.right, -rotY);
+    }
+
+
+}
---
+++ table_1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class table_1 : MonoBehaviour {
+	private InstantTrackerController trackerScript;
+	private GameObject ButtonsParent;
+
+
+	// Useing this for initialization
+	void Start () {
+		trackerScript = GameObject.Find ("Controller").gameObject.GetComponent<InstantTrackerController> ();
+		ButtonsParent = GameObject.Find ("Buttons Parent");
+
+		trackerScript._gridRenderer.enabled = false;//false
+		ButtonsParent.SetActive (true);//fasle
+
+	}
+
+	void OnEnable(){
+		    trackerScript._gridRenderer.enabled = false;//false
+			ButtonsParent.SetActive (true);//false
+
+	}
+
+	void OnDisable(){
+		ButtonsParent.SetActive (true);	
+	}
+}
